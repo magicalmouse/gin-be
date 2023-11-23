4bfed3a25c3dd11b8f8b6a89ad0143bf7d8f9302
thimba831 <ocbmage@outlook.com>
2023-11-22 16:22:05 -0800

    add the seperate controller

--- gin-be.go
+++ gin-be.go
@@ -1,43 +1,22 @@
 package main
 
 import (
-	"log"
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
-func LoggerMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		start := time.Now()
-		ctx.Next()
-		duration := time.Since(start)
-		log.Printf("Request - Method: %s | Status: %d | Duration: %v", ctx.Request.Method, ctx.Writer.Status(), duration)
-	}
+type UserController struct{}
+
+func (uc *UserController) GetUserInfo(c *gin.Context) {
+	userID := c.Param("id")
+	c.JSON(200, gin.H{"id": userID, "name": "John Doe", "email": "john@example.com"})
 }
 
-func AuthMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		apiKey := ctx.GetHeader("API-Key")
-		if apiKey == "" {
-			ctx.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized"})
-			return
-		}
-		ctx.Next()
-	}
-}
 func main() {
 	router := gin.Default()
 
-	//router.Use(LoggerMiddleware())
-	authGroup := router.Group("/api")
+	UserController := &UserController{}
 
-	authGroup.Use(AuthMiddleware())
-	{
-		authGroup.GET("/data", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"message": "Authenticated and authrized!"})
-		})
-	}
+	router.GET("/users/:id", UserController.GetUserInfo)
 
 	router.Run(":8080")
 }
